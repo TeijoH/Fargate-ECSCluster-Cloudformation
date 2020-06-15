a0aea75fcd0f5d3102459450581117772226924c
Teijo Heimola <teijo.heimola@gofore.com>
2020-06-15 19:04:20 +0300

    Some more editing

--- server.js
+++ server.js
@@ -3,13 +3,13 @@
 const express = require('express');
 
 // Constants
-const PORT = 8080;
+const PORT = 8081;
 const HOST = '0.0.0.0';
 
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World Version 1');
+  res.send('Hellouuu World Version 1');
 });
 
 app.listen(PORT, HOST);
